5c64cfccf6ef30dd72639b65759301568e32273d
Kyle Boutette <kyleveb@gmail.com>
2025-01-20 19:45:11 +0000

    encode_audio enable concurrency
    It was slow, now its unsatisfying too fast. Alas

--- salt/tools/bass_transcode/encode_audio.sh
+++ salt/tools/bass_transcode/encode_audio.sh
@@ -1,4 +1,20 @@
 # tweaked silence removal...
 export IFS=$'\n'
 mkdir -p audio_output
-for i in $(cat list); do echo "${i}" && nameis=${i%.*} && echo $nameis && ffmpeg -y -i "${i}" -af "areverse,atrim=start=0.2,silenceremove=start_periods=1:start_silence=0.3:start_threshold=0.03,areverse,atrim=start=0.2,silenceremove=start_periods=1:start_silence=0.3:start_threshold=0.03" -f opus audio_output/${nameis}.opus ; done
+concurrency=8
+counter=0
+for i in $(cat list); do
+        echo "${i}"
+        nameis=${i%.*}
+        echo $nameis
+        ffmpeg -y -i "${i}" -af "areverse,atrim=start=0.2,silenceremove=start_periods=1:start_silence=0.3:start_threshold=0.03,areverse,atrim=start=0.2,silenceremove=start_periods=1:start_silence=0.3:start_threshold=0.03" -f opus audio_output/${nameis}.opus &
+        counter=$((counter+1))
+        # Note: paralleism here is not the best, this waits for an entire batch
+        # of N to complete before proceeding to the next batch.
+        if [ $counter -eq $concurrency ]; then
+                wait
+                counter=0
+        fi
+done
+
+wait
